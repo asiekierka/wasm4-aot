fafab1505d39151eac562169bdce70bc1b9cb796
Adrian Siekierka <kontakt@asie.pl>
2022-10-15 10:57:38 +0200

    gba: fix timing, key presses

--- src/platform/gba/platform.c
+++ src/platform/gba/platform.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <tonc.h>
+#include <tonc_bios.h>
 #include <tonc_memdef.h>
+#include <tonc_memmap.h>
 #include "runtime.h"
 
 EWRAM_BSS
@@ -11,21 +13,33 @@ const uint32_t bpp_2_8_table[256] = {
 #include "fb_table.inc"
 };
 
+static uint16_t held_keys, vbl_ticks;
+
+void platform_irq_vblank() {
+    held_keys |= ~REG_KEYINPUT;
+    vbl_ticks++;
+    REG_IF = IRQ_VBLANK;
+}
+
 void platform_init(void) {
+	irq_init(NULL);
+	irq_add(II_VBLANK, platform_irq_vblank);
+
     REG_BG2CNT = 0;
     REG_DISPCNT = DCNT_MODE4 | DCNT_BG2;
 }
 
 void platform_update(void) {
-    key_poll();
-    uint32_t held_keys = key_held(-1);
+    REG_IME = 0;
     uint8_t w4_held_keys =
         ((held_keys & KEY_UP) ? W4_BUTTON_UP : 0)
-         |((held_keys & KEY_DOWN) ? W4_BUTTON_DOWN : 0)
+        | ((held_keys & KEY_DOWN) ? W4_BUTTON_DOWN : 0)
         | ((held_keys & KEY_RIGHT) ? W4_BUTTON_RIGHT : 0)
         | ((held_keys & KEY_LEFT) ? W4_BUTTON_LEFT : 0)
         | ((held_keys & KEY_B) ? W4_BUTTON_X : 0)
         | ((held_keys & KEY_A) ? W4_BUTTON_Z : 0);
+    held_keys = 0;
+    REG_IME = 1;
     w4_runtimeSetGamepad(0, w4_held_keys);
 }
 
@@ -46,6 +60,13 @@ void platform_draw(void) {
             *vram = bpp_2_8_table[*src];
         }
     }
+
+    if (vbl_ticks == 0) {
+        VBlankIntrWait();
+    }
+    REG_IME = 0;
+    vbl_ticks = 0;
+    REG_IME = 1;
 }
 
 void platform_draw_partial(int x, int y, int width, int height) {
